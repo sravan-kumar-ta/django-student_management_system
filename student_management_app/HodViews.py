6b27ebafa2f1b94d1fd480689f0273181433dfd8
sravan-kumar-ta <sravan0095@gmail.com>
2021-12-15 11:30:10 +0530

    add student

--- student_management_app/HodViews.py
+++ student_management_app/HodViews.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.urls import reverse
 
 from student_management_app.forms import AddStudentForm
-from student_management_app.models import CustomUser, Courses
+from student_management_app.models import CustomUser, Courses, Subjects
 
 
 def admin_home(request):
@@ -91,3 +91,29 @@ def add_student_save(request):
         except:
             messages.error(request, "Failed to Add Student")
             return HttpResponseRedirect("/add_student")
+
+
+def add_subject(request):
+    courses = Courses.objects.all()
+    staffs = CustomUser.objects.filter(user_type=2)
+    return render(request, "hod_template/add_subject_template.html", {"staffs": staffs, "courses": courses})
+
+
+def add_subject_save(request):
+    if request.method != "POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+        subject_name = request.POST.get("subject_name")
+        course_id = request.POST.get("course")
+        course = Courses.objects.get(id=course_id)
+        staff_id = request.POST.get("staff")
+        staff = CustomUser.objects.get(id=staff_id)
+
+        try:
+            subject = Subjects(subject_name=subject_name, course_id=course, staff_id=staff)
+            subject.save()
+            messages.success(request, "Successfully Added Subject")
+            return HttpResponseRedirect(reverse("add_subject"))
+        except:
+            messages.error(request, "Failed to Add Subject")
+            return HttpResponseRedirect(reverse("add_subject"))
